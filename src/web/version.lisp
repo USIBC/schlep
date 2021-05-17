93d497636b9a7d0e6a2a3e9a90ace547a97835b6
Darren Racine <dracine@ibc.doi.gov>
2021-05-17 11:38:18 -0600

    Fix indent

--- src/web/version.lisp
+++ src/web/version.lisp
@@ -8,7 +8,7 @@
   "Read and return app id's version from its About screen"
   (with-form (f *about-path* id)
     (let ((elts (remove-if-not (lambda (x) (string-equal (attribute x "class") "form_items"))
-                (get-elements-by-tag-name (form-dom f) "span"))))
+                               (get-elements-by-tag-name (form-dom f) "span"))))
       (if (> (length elts) 0)
         (text (car (last elts)))
         (values nil "Version scrape failed")))))
